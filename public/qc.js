af2469d3256386510eb217f4f3ad5853f74754e1
Charles Julian Knight <charles@rabidaudio.com>
2015-01-23 23:17:23 -0500

    replaced test page with Bootstrap blog example

--- public/qc.js
+++ public/qc.js
@@ -1,1 +1,27 @@
-console.log("meow")
+var FB = new Firebase('cjk-blog.firebaseio.com');
+
+var auth = function(provider){
+  switch(provider){
+    case "anonymous":
+      FB.authAnonymously(function(err, a){});
+      break;
+
+    case "password":
+      //popup username+password
+      //FB.authWithPassword
+      break;
+
+    case "facebook":
+    case "google":
+    case "twitter":
+    case "github":
+      FB.authWithOAuthPopup(provider, function(err, a){});
+      break;
+    default:
+      throw "Invalid auth type";
+  }
+}
+
+$('.gc-new-body').focus(function(){
+  $(this).attr('rows', 5).off('focus');
+});
